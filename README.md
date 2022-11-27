8037733e7e53d6a7d71bbc1a5523ab3208defaf5
Kliment Semushev <semush.kliment@gmail.com>
2022-11-27 16:46:56 +0300

    weather and jokes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # The Talking Emae bot for Telegram
-@@ test project to explore possibilities using DialogFlow in Python @@
+@@ test project to explore possibilities using spaCy in Python @@
 
-+ creation stage: tired of unsuccesses :(
-+ plans: telling weather and jokes ;)
++ creation stage: bot can tell weather (if location provided in the same message) and jokes (updated daily)
++ plans: improve the weather telling
 
 ---
 [Telegram Bot](http://t.me/talking_emae_bot)
--- bot.py
+++ bot.py
@@ -5,7 +5,14 @@ import logging
 import asyncio
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import spacy
+from datetime import datetime
+import random
+from bs4 import BeautifulSoup
+import collections
 
+collections.Callable = collections.abc.Callable
+# that's essential for correct work of BeautifulSoup.find_all() method
 
 tokens = {}
 with open("config.json", "r") as f:
@@ -17,6 +24,47 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+nlp = spacy.load("ru_core_news_lg")
+ruler = nlp.add_pipe("entity_ruler").from_disk("./patterns.jsonl")
+
+
+def get_weather(location):
+    geolocator = Nominatim(user_agent='weather-bot')
+    location = geolocator.geocode(location)
+    lat = location.latitude
+    long = location.longitude
+    weather_req = requests.get('https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'.format(lat, long, tokens["openweather-api-token"]))
+    weather = json.loads(weather_req.text)
+    if weather['cod'] < 299:
+        temp = round(weather['main']['temp'] - 273.15)
+        feels_like = round(weather['main']['feels_like'] - 273.15)
+        clouds = weather['clouds']['all']
+        wind_speed = weather['wind']['speed']
+
+        return 'Сейчас температура воздуха - {} градусов, ощущается как {} градусов, облачность - {}%, скорость ветра - {}м/с'.format(str(temp), str(feels_like), str(clouds), str(wind_speed))
+    else:
+        return 'Извините, сервис погоды временно умер...'
+      
+def tell_a_joke():
+    date = datetime.now().strftime("%Y-%m-%d")
+    jokes_json = {}
+    with open("jokes.json", "r") as f:
+        jokes_json = json.load(f)
+    if jokes_json['date'] != date:
+        page = requests.get('http://anekdotov.net/anekdot/day/').text
+        soup = BeautifulSoup(page, 'lxml')
+        divs = soup.body.find_all('div', class_='anekdot')
+        jokes_parsed = []
+        for div in divs:
+            jokes_parsed.append(div.text)
+        jokes_json['date'] = date
+        jokes_json['jokes'] = jokes_parsed
+        with open("jokes.json", "w") as f:
+            json.dump(jokes_json, f)
+        return random.choice(jokes_parsed)
+    else:
+        return random.choice(jokes_json['jokes'])
+    
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -36,7 +84,25 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 async def text_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    await update.message.reply_text(f"Ваш ID: {update.message.from_user.id}")
+    doc = nlp(update.message.text)
+    q_weather = False
+    q_loc = []
+    q_joy = False
+    for ent in doc.ents:
+        if ent.label_ == "WEATHER":
+            q_weather = True
+        elif ent.label_ == "JOY":
+            q_joy = True
+        elif ent.label_ == "LOC":
+            q_loc.append(ent.text)
+        
+    if q_weather & (len(q_loc) > 0):
+        await update.message.reply_text(get_weather(q_loc[0]))
+    elif q_joy:
+        await update.message.reply_text(tell_a_joke())
+    else:
+        await update.message.reply_text("Я не понимаю, что вы от меня хотите :(")
+        return
 
 
 def main() -> None:
@@ -55,26 +121,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-
-    '''req = request.get_json(force=True)
-
-    action = req.get('queryResult').get('action')
-
-    result = req.get("queryResult")
-    parameters = result.get("parameters")
-
-    if parameters.get('location').get('city'):
-        geolocator = Nominatim(user_agent='weather-bot')
-        location = geolocator.geocode(parameters.get('location').get('city'))
-        lat = location.latitude
-        long = location.longitude
-        weather_req = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(lat, long, tokens["openweather-api-token"]))
-        current_weather = json.loads(weather_req.text)['current']
-        temp = round(current_weather['temp'] - 273.15)
-        feels_like = round(current_weather['feels_like'] - 273.15)
-        clouds = current_weather['clouds']
-        wind_speed = current_weather['wind_speed']
-
-    return {'fulfillmentText': 'Сейчас температура воздуха - {} градусов, ощущается как {} градусов, облачность - {}%, скорость ветра - {}м/с'.format(str(temp), str(feels_like), str(clouds), str(wind_speed))}'''
-
